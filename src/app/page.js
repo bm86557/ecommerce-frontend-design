83155752577d0e071fb5ad959039b8c77db1f5b7
Bilal Malik <bm8244788@gmail.com>
2026-05-18 17:04:17 +0500

    fix the route

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
-import Link from "next/link";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', fontFamily: 'Arial, sans-serif' }}>
-      <h2>Assignment Task - E-commerce Website</h2><br />
-      <p><Link href={'/web-main'}>Main Page</Link></p>
-    </div>
-  );
+  redirect('/web-main');
 }
